e0a321d8fa696e9d3263c550b7ff93d1fcfbf897
Chad <cdsmith7@gmail.com>
2024-10-28 22:04:10 -0500

    create client with jakarta

---
+++ src/main/java/com/pluralsight/client/SpeakerClient.java
@@ -0,0 +1,29 @@
+package com.pluralsight.client;
+import com.pluralsight.model.Speaker;
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.core.MediaType;
+
+public class SpeakerClient {
+    private Client client;
+    private final String SPEAKER_URI = "http://localhost:8080/speaker";
+
+    public SpeakerClient(){
+        client = ClientBuilder.newClient();
+    }
+
+    public Speaker get (Long l){
+        return client
+                .target(SPEAKER_URI)
+                .path(String.valueOf(l))
+                .request(MediaType.APPLICATION_JSON)
+                .get(Speaker.class);
+    }
+
+    public static void main (String[] args){
+        SpeakerClient client = new SpeakerClient();
+        Speaker speaker = client.get(1L);
+
+        System.out.println(speaker.getName());
+    }
+}
