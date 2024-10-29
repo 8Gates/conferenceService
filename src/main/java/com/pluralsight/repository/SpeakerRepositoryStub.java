9765fa5465a83cda46156b928012fe35b7cee967
Chad <cdsmith7@gmail.com>
2024-10-28 21:42:23 -0500

    enable create new speaker via post

--- src/main/java/com/pluralsight/repository/SpeakerRepositoryStub.java
+++ src/main/java/com/pluralsight/repository/SpeakerRepositoryStub.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SpeakerRepositoryStub implements SpeakerRepository {
-    private final List<Speaker> speakers = new ArrayList<>();
+    private static List<Speaker> speakers = new ArrayList<>();
     public SpeakerRepositoryStub(){
         // no argument constructor for testing purposes
         Speaker speaker1 = new Speaker();
@@ -29,6 +29,13 @@ public class SpeakerRepositoryStub implements SpeakerRepository {
     @Override
     public Speaker findById(Long id) {
         return findSpeakerById(speakers, id);
+    }
+
+    @Override
+    public Speaker create(Speaker speaker) {
+        speaker.setId(speakers.size() + 1L);
+        speakers.add(speaker);
+        return speaker;
     }
 
     private Speaker findSpeakerById(List<Speaker> speakers, Long id) {
